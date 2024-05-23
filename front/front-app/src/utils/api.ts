58b00031e14deefdd926c6afc5dd13a6f33addad
unknown <jejosu346@gmail.com>
2024-05-23 17:06:30 -0300

    criação de um modulo de API para lidar com as requisições

--- front/front-app/src/utils/api.ts
+++ front/front-app/src/utils/api.ts
@@ -1,5 +1,22 @@
 import axios from "axios";
 
+import { TaskType } from "../types/Task";
+import { SetStateAction } from "react";
+
 export const api = axios.create({
   baseURL:  'http://localhost:3001'
 });
+
+export async function getTasks (user: string, setList: React.Dispatch<SetStateAction<TaskType[]>>){
+  await api.get(`/user/${user}/tasks`)
+      .then(({data}) => {
+        setList(data);
+      }).catch(() => []);
+}
+
+export async function getUser (setUser: React.Dispatch<SetStateAction<string>>){
+  await api.get(`/users`)
+      .then(({data}) => {
+        setUser(data[0]);
+      }).catch(() => []);
+}
