dc1043b5941132b72a5673bfba57a9c40aa388e4
unknown <jejosu346@gmail.com>
2024-05-23 17:24:36 -0300

    adicao do botao de troca de modo de visualizacao

--- front/front-app/src/components/MontlyTasks/index.tsx
+++ front/front-app/src/components/MontlyTasks/index.tsx
@@ -1,9 +1,11 @@
+import { TasksModeButton } from "../TasksModeButton";
 import { TasksContainerStyle } from "./styles";
 
 export function MontlyTasks() {
   return (
     <TasksContainerStyle>
       <h1>TASKS POR MES</h1>
+      <TasksModeButton></TasksModeButton>
     </TasksContainerStyle>
   );
 }
